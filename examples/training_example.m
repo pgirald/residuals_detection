fa05638e96c0c9dbcb295bc6cf0e81ec4c843af8
unknown <pgirald18@gmail.com>
2023-06-29 09:28:57 -0500

    Support minimum redundancy and maximun relevancy for training

--- examples/training_example.m
+++ examples/training_example.m
@@ -5,36 +5,36 @@ close all;
 clusters = 4;
 
 colors = [255 0 0
+            255 255 0
             0 255 0
             0 0 255
-            255 255 0
             %255 0 255
             %0 255 255
             ];
 
+bestfeats = dictionary({'tdstats','haralick', 'shape'}, [5, 1, 1]);
+%{
 ftnames = {'tdstats', 'haralick', 'shape'};
 
 ftlabels = {'Estadísticas', 'Haralick', {'Descriptores', 'de Forma'}};
-%{
+%}
 ftnames = {'cspclasses', 'sampledsignal', 'utvangs'};
 
 ftlabels = {'Splines naturales', 'Muestreo de señal', 'Ángulos VTU'};
-%}
+
 
 aux = [ftnames, 'be'];
 
 feats = load('features_sim.mat', aux{:});
+%{
 feats.shape = feats.shape(:, {'Elongation', 'Major axis length', 'Entropy'});
 feats.shape.be_r = feats.be(:, 1);
 feats.shape.be_g = feats.be(:, 2);
 feats.shape.be_b = feats.be(:, 3);
+%}
 load data\sequence_sim.mat imgs maskind low normal high critical;
 
-%feats.shape = feats.shape(:, {'Total curvature'});
-
 centroids = cell(1, numel(ftnames));
-
-%feats.cspclasses = feats.cspclasses(:, [1 2 4 5 6 8 9 10 12]); 
 
 %{
 cols = {'Mean','RMS','StandardDeviation','ShapeFactor','SINAD','SHR',...
@@ -64,12 +64,50 @@ fslabels = [strcat(cols, '_red'),...
 
 %feats.utvangs = [std(feats.utvangs(:, 1:361),0, 2), std(feats.utvangs(:, 362:722), 0,2), std(feats.utvangs(:, 723:1083),0,2)];
 
+[rows, cols] = size(imgs, [1 2]);
+
+zoneslbs = zeros(rows * cols, 1);
+
+zoneslbs(find(low ~= 0)) = 1;
+zoneslbs(find(normal ~= 0)) = 2;
+zoneslbs(find(high ~= 0)) = 3;
+zoneslbs(find(critical ~= 0)) = 4;
+
+expectedR = zeros(rows, cols);
+expectedG = zeros(rows, cols);
+expectedB = zeros(rows, cols);
+
+expectedR(critical ~= 0) = 255;
+
+expectedR(high ~= 0) = 255;
+expectedG(high ~= 0) = 255;
+
+expectedG(normal ~= 0) = 255;
+
+expectedB(low ~= 0) = 255;
+
+expected = cat(3,expectedR, expectedG, expectedB);
+
+bestIndices = struct;
+%{
+[idx, scores] = fscmrmr(feats.cspclasses(zoneslbs ~= 0, :), zoneslbs(zoneslbs ~= 0));
+
+feats.cspclasses = feats.cspclasses(: ,idx(1:2));
+%}
 for i = 1:numel(ftnames)
     if isa(feats.(ftnames{i}), 'table')
+        [idx, scores] = fscmrmr(feats.(ftnames{i})(zoneslbs ~= 0, :), zoneslbs(zoneslbs ~= 0));
+        bestIndices.(ftnames{i}) = [feats.(ftnames{i}).Properties.VariableNames(idx);...
+            num2cell(idx);num2cell(scores(idx))];
         feats.(ftnames{i}) = table2array(feats.(ftnames{i}));
+        if isKey(bestfeats, {ftnames{i}})
+            endfeat = bestfeats({ftnames{i}});
+        else
+            endfeat = numel(idx);
+        end
+        feats.(ftnames{i}) = feats.(ftnames{i})(:, idx(1: endfeat));
     end
 end
-
 
 channels = 3;
 
@@ -83,7 +121,7 @@ tiledlayout('flow');
 
 nexttile, imshow(img), title('Original');
 
-[rows, cols] = size(imgs, [1 2]);
+nexttile, imshow(expected), title({'Resultado', 'esperado'});
 
 for i = 1:numel(ftnames)
     x = feats.(ftnames{i});
